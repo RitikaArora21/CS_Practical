19c8f95bb3701a0ddd304dc6edf3361984448b56
RitikaArora21 <97821587+RitikaArora21@users.noreply.github.com>
2022-03-10 22:30:13 +0530

    Update Q.9.py

--- Q.9.py
+++ Q.9.py
@@ -9,3 +9,13 @@ while(n > 0):
     n = n // 10
  
 print(rev)
+def getSum(n):
+
+    sum = 0
+    while (n != 0):
+
+        sum = sum + int(n % 10)
+        n = int(n/10)
+
+    return sum
+getSum(56)
