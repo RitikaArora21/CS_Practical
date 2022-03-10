0b2522232a7992076066d0c4137ea900c4ee6a5d
RitikaArora21 <97821587+RitikaArora21@users.noreply.github.com>
2022-03-10 23:33:28 +0530

    Update Q.23.py

--- Q.23.py
+++ Q.23.py
@@ -13,3 +13,34 @@ for i in range(n-1):
             a[i] = a[j]
             a[j] = t
 print("Sorted array by selection sort : ",a)
+#Insertion sort
+n = int(input("Enter the length of the list : "))
+a = []
+for i in range(n):
+    s = int(input("Enter the value : "))
+    a.append(s)
+
+for i in range(1,n):
+    t = a[i]
+    j = i-1
+    while j >= 0 and t < a[j]:
+        a[j+1] = a[j]
+        j = j-1
+    a[j+1] = t
+print("Sorted array by insertion sort : ",a)
+                            
+#Bubble Sort                       
+
+n = int(input("Enter the length of the list : "))
+a = []
+for i in range(n):
+    s = int(input("Enter the value : "))
+    a.append(s)
+
+for i in range(n):
+    for j in range(0,n-i-1):
+        if a[j] > a[j+1] :
+            t = a[j]
+            a[j] = a[j+1]
+            a[j+1] = t 
+print("Sorted array by bubble sort : ",a)
